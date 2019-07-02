d6e8a5335e61608e2d2e920f26af6c285e93d626
yodtai <52165254+yodtai@users.noreply.github.com>
2019-07-02 21:39:46 +0700

    Add files via upload

---
+++ ChangeMoney.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int Moneybath;
+	float dollar=30.78;
+	float euro =34.94;
+	cout<<"input Thai Baths : ";
+	cin>>Moneybath;
+    cout<<Moneybath<<" Baths is  "<<Moneybath/dollar<<" dollars"<<endl;
+	cout<<Moneybath<<" Baths is  "<<Moneybath/euro<<" Euro."<<endl;
+
+    return 0 ;
+}
